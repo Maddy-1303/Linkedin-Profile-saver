ffa836896e447574142158200930babb333682f4
Madhan <madhanprasath@mallow-tech.com>
2023-12-15 16:46:20 +0530

    Created to a mid level

--- popup.js
+++ popup.js
@@ -1,11 +1,80 @@
-document.getElementById("saveProfileBtn").addEventListener("click", function () {
-    // Replace this array with the LinkedIn profile URLs you want to save
-    const profileUrls = [
-      "https://www.linkedin.com/in/ACwAAANumBsBo_bC5qa05OJPlHivQCu4qGu7ato",
-      "https://www.linkedin.com/in/ACwAAAbwEXYBUtlx2prN622F_dJqMx9AauHO3Qk"
-    ];
+document.getElementById("openProfileBtn").addEventListener("click", function () {
+    // Load the LinkedIn profile URLs from the JSON file
+    fetch('linkedin_urls.json')
+      .then(response => response.json())
+      .then(data => {
+        const linkedinUrls = data.linkedinUrls;
   
-    // Send a message to the background script to save the profile data
-    chrome.runtime.sendMessage({ action: "saveProfileData", data: profileUrls });
+        // Validate each LinkedIn profile URL using a simple regex
+        const validUrls = linkedinUrls.filter(profileUrl => {
+          const urlRegex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9_-]+\/?$/;
+          return urlRegex.test(profileUrl);
+        });
+  
+        // Check if any URLs are invalid
+        if (validUrls.length !== linkedinUrls.length) {
+          console.error("Some LinkedIn profile URLs are invalid");
+          displayErrorMessage("Some LinkedIn profile URLs are invalid");
+          return;
+        }
+  
+        // Create a folder for saving the files
+        const folderName = 'LinkedInProfiles';
+        chrome.downloads.download({
+          url: 'blob:null', // Using a dummy URL
+          filename: folderName,
+          conflictAction: 'overwrite',
+          saveAs: true,
+        }, function (downloadId) {
+          if (chrome.runtime.lastError) {
+            console.error('Error creating folder:', chrome.runtime.lastError);
+            displayErrorMessage('Error creating folder');
+            return;
+          }
+  
+          // Iterate through each valid LinkedIn profile URL and initiate the download
+          validUrls.forEach(profileUrl => {
+            // Save the profile URL to a text file in the folder
+            const data = `LinkedIn Profile URL: ${profileUrl}`;
+            const blob = new Blob([data], { type: 'text/plain' });
+  
+            // Create a temporary URL for the blob
+            const url = URL.createObjectURL(blob);
+  
+            // Use chrome.downloads.download to save the file in the folder
+            chrome.downloads.download({
+              url: url,
+              filename: `${folderName}/${getFileName(profileUrl)}`,
+              conflictAction: 'overwrite',
+              saveAs: false
+            }, function (downloadId) {
+              // Clean up the temporary URL
+              URL.revokeObjectURL(url);
+              console.log("File download initiated with ID:", downloadId);
+            });
+          });
+        });
+      })
+      .catch(error => {
+        console.error('Error loading LinkedIn profile URLs:', error);
+        displayErrorMessage('Error loading LinkedIn profile URLs');
+      });
   });
+  
+  function displayErrorMessage(message) {
+    // Display an error message in the popup
+    const errorMessageContainer = document.getElementById("errorMessage");
+    errorMessageContainer.textContent = message;
+  
+    // Clear the error message after a short delay
+    setTimeout(() => {
+      errorMessageContainer.textContent = '';
+    }, 3000); // 3000 milliseconds (3 seconds)
+  }
+  
+  function getFileName(url) {
+    // Extract the last part of the URL as the filename
+    const parts = url.split('/');
+    return parts[parts.length - 1] + '.txt';
+  }
   
